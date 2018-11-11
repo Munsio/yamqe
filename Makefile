d0432ecae64c125f02b236fb9a9c32cca50fee46
munsio <github@r2pi.net>
2018-11-11 22:41:09 +0100

    make better makefiles and scripts for building

--- Makefile
+++ Makefile
@@ -1,29 +1,30 @@
 PROJECT := yamqe
 BINDIR  := $(CURDIR)/bin
-VERSION := `cat VERSION`
+VERSION := $(shell cat VERSION)
+XC_OS 	:= linux darwin windows
+XC_ARCH := 386 amd64 arm
+
+export XC_OS
+export XC_ARCH
+export VERSION
 
 .PHONY: all
 all: help
 
+
 .PHONY: help
 help:
 	@echo "make test - run go test"
-	@echo "make build - build yamqe"
-	@echo "make build-travis - compiles binaries for x64 mac/linux and creates release tar.gz files with hashsums"
+	@echo "make build - build yamqe for follwing OS-ARCH constilations: $(XC_OS) / $(XC_ARCH) "
+	@echo "make build-dev - build yamqe for OS-ARCH set by GOOS and GOARCH env variables"
+	@echo "make dist - build and create packages with hashsums"
+
 
 .PHONY: build
-
 build:
-	@scripts/build.sh $(VERSION)
+	@scripts/build.sh
 
-build-travis: 
-	$(eval FILES := $(shell ls build))
-	@for f in $(FILES); do \
-		(cd $(shell pwd)/build/$$f && tar -cvzf ../../dist/$$f.tar.gz *); \
-		(cd $(shell pwd)/dist && shasum -a 256 $$f.tar.gz > $$f.sha256); \
-		(cd $(shell pwd)/dist && md5sum $$f.tar.gz > $$f.md5); \
-		echo $$f; \
-	done
 
-build-dir:
-	@rm -rf build && mkdir build
+.PHONY: dist
+dist: 
+	@scripts/dist.sh
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,48 @@
 set -e
 
 # Get the version from the command line
-VERSION=$1
 if [ -z $VERSION ]; then
     echo "Please specify a version."
     exit 1
 fi
 
+# If its dev mode, only build for ourself
+if [ "$VERSION" == "dev" ]; then
+    XC_OS=$(go env GOOS)
+    XC_ARCH=$(go env GOARCH)
+fi
 
-echo $VERSION
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 
-#GOOS=linux GOARCH=amd64 go build -o build/linux-amd64/yamqe ./cmd/yamqe
+# Change into that directory
+cd "$DIR"
+
+echo "## Recreate directory..."
+rm -rf build && mkdir build
+
+# instruct to build statically linked binaries
+export CGO_ENABLED=0
+
+# build cmds
+echo "## Build..."
+if [ -d ./cmd ]; then
+    for cmd in ./cmd/*; do
+        if [ ! -d "$cmd" ]; then
+            continue
+        fi
+        for OS in $XC_OS; do
+            for ARCH in $XC_ARCH; do
+                if ([ $OS == "darwin" ] && ([ $ARCH == "386" ] || [ $ARCH == "arm" ])) ||
+                ([ $OS == "windows" ] && [ $ARCH == "arm" ])
+                then
+                    continue
+                fi
+                GOOS=$OS GOARCH=$ARCH go build -o build/$OS-$ARCH/${cmd##*/} ./cmd/${cmd##*/}
+                echo "- $OS-$ARCH finished"
+            done
+        done
+    done
+fi
